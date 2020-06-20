2b730af023ca34cc00fe2fdcf7a814f49cf4676a
derek rosas <derekianrosas@gmail.com>
2020-06-19 23:17:01 -0600

    added toggle but doesn't toggle

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row .seat:not(.occupied");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+
+const ticketPrice = +movieSelect.value; //+ changes value to number
+
+container.addEventListener("click", (e) => {
+  console.log(e);
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+  }
+});
